16115848f846ee1487e31388db49eab4b8c7f997
LAPTOP-JHLSCDA7\steve <steve@soaconsultingservices.com>
2018-04-19 09:12:22 -0400

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,15 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { AboutPage } from '../pages/about/about';
+import { ContactPage } from '../pages/contact/contact';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    AboutPage,
+    ContactPage
   ],
   imports: [
     BrowserModule,
@@ -19,7 +23,9 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    AboutPage,
+    ContactPage
   ],
   providers: [
     StatusBar,
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { ContactPage } from '../contact/contact';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,13 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  nextPage: any = ContactPage;
 
+  constructor(public navCtrl: NavController) {
+    console.log("HomePage navCtrl.length(): " + navCtrl.length());
   }
 
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad HomePage');
+  }
 }
